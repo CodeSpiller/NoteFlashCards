0e0625479075c5b9ebc092f89c7fe5357297ffb7
Quinten Spillemaeckers <quintenspillemaeckers@gmail.com>
2026-04-19 20:34:05 +0200

    added note names in hidden/active tab

--- build/_app/immutable/chunks/Cp2NaJl3.js
+++
@@ -1,1 +0,0 @@
-import{H as t}from"./-UyI9lYi.js";import"./CGd24-3-.js";function i(r,o){throw new t(r,o)}export{i as e};
---
+++ build/_app/immutable/entry/start.zEpFKFCt.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/B2yf1sxJ.js";import{z as t}from"../chunks/C5Wp7K9I.js";export{t as load_css,r as start};
