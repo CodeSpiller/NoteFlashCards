fea92350dd0014d1e68e6d654daabb31e7b4cdba
Quinten Spillemaeckers <quintenspillemaeckers@gmail.com>
2026-04-19 10:32:46 +0200

    long press fix for phone use

--- build/_app/immutable/entry/start.B-45j4jD.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/D5qFzqGt.js";import{z as t}from"../chunks/5O2EFi-6.js";export{t as load_css,r as start};
---
+++ build/_app/immutable/entry/start.G5BM7mqQ.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/DoOslOzS.js";import{z as t}from"../chunks/DVWBBSGb.js";export{t as load_css,r as start};
